89bf7c7ff2ad1079a765fc9305c332aea4fcc71f
Elshindr <headkeytics@gmail.com>
2022-10-24 16:00:55 +0200

    Profiles

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,23 @@
 package dev;
 
+import java.io.IOException;
+import java.util.ResourceBundle;
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!");
+    public static void main( String[] args ) throws IOException {
+        String titre = ResourceBundle.getBundle("application").getString("titre");
+        String asciiArt = FigletFont.convertOneLine(titre);
+        System.out.println(asciiArt);
+
+        // Affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString(
+                "environnement");
+        System.out.println("Environnement : " + environnement);
     }
 }
